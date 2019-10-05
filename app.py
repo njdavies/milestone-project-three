37f2b6252cc92087cdc90afb851d990efe8cd986
Nathan Davies <nathandavies06@hotmail.co.uk>
2019-10-05 11:28:13 +0100

    Removed redundant code from add recipes function in app.py

--- app.py
+++ app.py
@@ -133,7 +133,6 @@ def add_recipe():
     cake_types = mongo.db.recipes.distinct("type")
     cuisine_types = mongo.db.recipes.distinct("cuisine")
     healthy_types = mongo.db.recipes.distinct("healthy")
-    recipes = mongo.db.recipes.find()
     return render_template("addrecipe.html", recipes=recipes, cakeTypes=cake_types, cuisineTypes=cuisine_types, healthyTypes=healthy_types)
 
 
